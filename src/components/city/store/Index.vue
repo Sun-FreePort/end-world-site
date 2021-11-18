4e96f795c4032c1124e0f4922538ad640823a979
UioSun <uio.sun@soundws.com>
2021-11-18 20:52:05 +0800

    interim

--- src/components/city/store/Index.vue
+++ src/components/city/store/Index.vue
@@ -12,7 +12,7 @@
     </w-progress>
     <w-flex class="blue-light5--bg pa1 wrap background">
       <Goods v-for="item in goods"
-             class="mr4"
+             class="mr3"
              :key="item.id"
              :id="item.id"
              :index="item.index"
